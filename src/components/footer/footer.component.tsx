d0bf8caa5b5e424049cdcc4230fb4bd6f8e84c65
ttyt0416 <ttyt0416@gmail.com>
2021-07-13 16:57:29 +0900

    third commit

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -1,8 +1,69 @@
 import React from "react";
 import "./footer.style.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 const Footer: React.FC = () => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="footer__iconcontainer">
+        <p className="footer__follow">FOLLOW</p>
+        <div className="footer__icons">
+          <ul className="footer__iconlist">
+            <li>
+              <FontAwesomeIcon icon={faTwitter} className="footer__icon" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faFacebook} className="footer__icon" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faInstagram} className="footer__icon" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faGithub} className="footer__icon" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faPhone} className="footer__icon" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faEnvelope} className="footer__icon" />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer__formcontainer">
+        <p className="footer__title">GET IN TOUCH</p>
+        <form className="footer__form">
+          <input
+            type="name"
+            placeholder="Name"
+            className="footer__form-input footer__form-name"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="footer__form-input footer__form-email"
+          />
+          <input
+            type="text"
+            placeholder="Message"
+            className="footer__form-input footer__form-text"
+          />
+          <input type="submit" value="SEND" className="footer__form-submit" />
+        </form>
+        <p className="footer__subtitle">
+          © Untitled. All rights reservedDesign: HTML5 UP
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
